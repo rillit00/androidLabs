71a4e3c0594b4888cd4f64d89f95feb3433b425b
Александра <adanilova@smart-consulting.ru>
2021-10-16 22:50:34 +0300

    LAB-2 create click listener, improve ui

--- firstproject/src/main/java/com/androidlabs/firstproject/MainActivity.java
+++ firstproject/src/main/java/com/androidlabs/firstproject/MainActivity.java
@@ -1,15 +1,19 @@
 package com.androidlabs.firstproject;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
+    Button btn1;
+    Button btn2;
+    Button btn3;
     TextView textView;
 
     @Override
@@ -18,18 +22,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = findViewById(R.id.textView);
-        btn = findViewById(R.id.btn);
-
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
 
         View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @SuppressLint({"ResourceAsColor", "SetTextI18n", "NonConstantResourceId"})
             @Override
             public void onClick(View v) {
-                // Меняем текст в TextView (tvOut)
-                textView.setText("Кнопка");
+                switch (v.getId()) {
+                    case R.id.btn1:
+                        textView.setText("Change text");
+                        break;
+                    case R.id.btn2:
+                        btn2.setBackgroundColor(R.color.purple_200);
+                        break;
+                    case R.id.btn3:
+                        Toast toast = Toast.makeText(getApplicationContext(),
+                                "Третья кнопка нажата!", Toast.LENGTH_SHORT);
+                        toast.show();
+                        break;
+                }
             }
         };
 
-        btn.setOnClickListener(oclBtnOk);
+        btn1.setOnClickListener(oclBtnOk);
+        btn2.setOnClickListener(oclBtnOk);
+        btn3.setOnClickListener(oclBtnOk);
     }
 
 
